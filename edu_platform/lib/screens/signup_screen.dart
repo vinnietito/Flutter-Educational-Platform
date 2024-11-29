0b6a21eee170f982d904fc686ce610596ec26665
vinnietito <vincentnjuguna69@gmail.com>
2024-11-29 14:36:24 +0300

    Commits

---
+++ edu_platform/lib/screens/signup_screen.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class SignupScreen extends StatefulWidget {
+  @override
+  _SignupScreenState createState() => _SignupScreenState();
+}
